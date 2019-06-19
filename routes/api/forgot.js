b547445c8f9ba6d4d9e99b05ee468609784deb98
Abhinanth <virtualabi2.0@gmail.com>
2019-06-19 15:59:57 +0530

    Validation for forgotPassword

--- routes/api/forgot.js
+++ routes/api/forgot.js
@@ -5,8 +5,29 @@ const router = express.Router();
 const User = require("../../models/User");
 
 router.post("/", (req, res) => {
-    console.log("forgot password", req.body);
+  console.log("forgot password", req.body);
 
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        userid: ID,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        otp: OTP
+      });
+    }
+  });
 });
 
 module.exports = router;
