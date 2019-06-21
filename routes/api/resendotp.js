2a1590581b718bafd8b07347d26ba9aeb60122a4
Abhinanth <virtualabi2.0@gmail.com>
2019-06-21 15:33:58 +0530

    Bug Fix

--- routes/api/resendotp.js
+++ routes/api/resendotp.js
@@ -11,35 +11,24 @@ const User = require("../../models/User");
 router.post("/", (req, res) => {
   console.log("Hello", req.body);
 
-  const { errors, isValid } = validateOtpInput(req.body);
-
-  // Check Validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
   let newOTP = common.generateOTP();
   let uid = new mongodb.ObjectId(req.body.userid);
 
-  User.find({ _id: uid, otp: req.body.otp }).then(user => {
+  User.find({ _id: uid }).then(user => {
     if (user) {
       console.log(user);
-      User.update(
-        { _id: uid },
-        { $set: { status: true, emailisverified: true, otp: newOTP } },
-        (err, result) => {
-          if (!err) {
-            res.status(200).json(
-              common.formatResponse({
-                type: "Activation",
-                code: "OTP and E-mail is Verified"
-              })
-            );
-          } else {
-            console.log(err);
-          }
+      User.update({ _id: uid }, { $set: { otp: newOTP } }, (err, result) => {
+        if (!err) {
+          res.status(200).json(
+            common.formatResponse({
+              type: "Activation",
+              code: "OTP and E-mail is Verified"
+            })
+          );
+        } else {
+          console.log(err);
         }
-      );
+      });
     }
   });
 });
